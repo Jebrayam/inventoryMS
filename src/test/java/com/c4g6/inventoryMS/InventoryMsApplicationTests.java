3bca4eb86a56a9ac49a58d5f4009f8845227af60
Jebrayam <brayamsaavedra@gmail.com>
2021-11-23 08:56:12 -0500

    Configuración inicial del proyecto

---
+++ src/test/java/com/c4g6/inventoryMS/InventoryMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c4g6.inventoryMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
